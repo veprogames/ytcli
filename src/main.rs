26a0869f94fc443b676f0af1524cdaf6966be092
veprogames <75524847+veprogames@users.noreply.github.com>
2023-04-30 10:11:21 +0200

    show prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> Result<(), String> {
+    loop {
+        print!("(ytcli) ");
+        if let Err(err) = io::Write::flush(&mut io::stdout()){
+            return Err(err.to_string());
+        }
+        let mut input = String::new();
+        if let Err(err) = io::stdin().read_line(&mut input) {
+            return Err(err.to_string());
+        }
+    }
 }
